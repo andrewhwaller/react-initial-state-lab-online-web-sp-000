295c6c675438de88e2da00360e247ed548c33abd
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 05:07:41 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 
 export default class ImageSlider extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.value
+    }
+  }
+
   render() {
     return (
       'I am on slide <CURRENT_SLIDE>'
